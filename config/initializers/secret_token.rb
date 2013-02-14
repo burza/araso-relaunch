ec74032a9e78f1f3e1fb9242a61a72b716363a23
Gregor <gregor.swatek@gmail.com>
2013-02-14 19:01:46 +0100

    first app commit, with rails 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArasoRelaunch::Application.config.secret_token = 'ec2994575cb9b5cc0f55d1ef70daf3509b6960297053aaa6b9e409c7744d37c9ac6a9dba3071a47d7b2f2fc1fedbea9400795e5f42289b62134993a46e210528'
