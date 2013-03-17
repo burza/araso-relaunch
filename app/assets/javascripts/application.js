c51c628ba6f9c047f362523cb159a68fffca2cbe
Gregor <gregor.swatek@gmail.com>
2013-03-17 12:21:47 +0100

    finished intro page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,72 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+
+// When ready...
+// window.addEventListener("load",function() {
+//   // Set a timeout...
+//   setTimeout(function(){
+//     // Hide the address bar!
+//     window.scrollTo(0, 1);
+//   }, 0);
+// });
+$(window).scroll(function () {
+  if($(this).scrollTop() == 0) {
+    $('.navbar').addClass('reset-navbar-styles');
+  } else if ($(this).scrollTop() >= 40) {
+    $('.navbar').removeClass('reset-navbar-styles');    
+  }
+});
+
+$(document).ready(function() {
+	$('.navbar .nav').onePageNav({
+    currentClass: 'active',
+    changeHash: true,
+    scrollSpeed: 750,
+    scrollOffset: 0,
+    scrollThreshold: 0.5,
+    filter: '',
+    easing: 'swing',
+
+    begin: function() {
+      //I get fired when the animation is starting
+    },
+    
+    end: function() {
+      //I get fired when the animation is ending
+    },
+    
+    scrollChange: function($currentListItem) {
+      //I get fired when you enter a section and I pass the list item of the section
+    }
+  });
+});
+
+(function( win ){
+	var doc = win.document;
+   	
+	// If there's a hash, or addEventListener is undefined, stop here
+	if( !location.hash && win.addEventListener ){
+		//scroll to 1
+		window.scrollTo( 0, 1 );
+		var scrollTop = 1,
+			getScrollTop = function(){
+				return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
+			},
+			//reset to 0 on bodyready, if needed
+			bodycheck = setInterval(function(){
+				if( doc.body ){
+					clearInterval( bodycheck );
+					scrollTop = getScrollTop();
+					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+				}	
+			}, 15 );
+		win.addEventListener( "load", function(){
+			setTimeout(function(){
+					//reset to hide addr bar at onload
+					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+			}, 0);
+		} );
+	}
+})( this );
+
